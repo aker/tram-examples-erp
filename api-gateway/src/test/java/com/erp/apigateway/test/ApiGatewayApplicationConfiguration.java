851ee6bcf451d251750ca98dfc5ae42350aa70d6
aker <kzjmail@gmail.com>
2018-09-12 07:59:54 +0800

    reupdate test files

---
+++ api-gateway/src/test/java/com/erp/apigateway/test/ApiGatewayApplicationConfiguration.java
@@ -0,0 +1,18 @@
+package com.erp.apigateway.test;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ApiGatewayApplicationConfiguration {
+
+    @Bean
+    public NettyReactiveWebServerFactory NettyReactiveWebServerFactory() {
+        return new NettyReactiveWebServerFactory();
+    }
+}
